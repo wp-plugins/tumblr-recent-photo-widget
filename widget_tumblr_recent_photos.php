1a3d7eec63c4f94cff6cbf7e207c3cc222cf8ccc
V.J.Catkick <V.J.Catkick@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-09-25 13:35:51 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/tumblr-recent-photo-widget/trunk@66246 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ widget_tumblr_recent_photos.php
@@ -0,0 +1,143 @@
+<?php
+/*
+Plugin Name: Tumblr Recent Photos Widget
+Plugin URI: http://www.vjcatkick.com/
+Description: Shows a list of recent photos from Tumber.
+Version: 0.0.2
+Author: V.J.Catkick
+Author URI: http://www.vjcatkick.com/
+*/
+
+/*
+Based on Display photos from Tumblr (using JSON method) entry at Drupal
+http://drupal.org/node/277619
+
+License: GPL
+Compatibility: WordPress 2.0 with Widget-plugin.
+
+Installation:
+Place the widget_tumblr_recent_photos.php file in your /wp-content/plugins/ directory
+and activate through the administration panel, and then go to the widget panel and
+drag it to where you would like to have it!
+*/
+
+/*  Copyright V.J.Catkick - http://www.vjcatkick.com/
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+
+/* Changelog
+* Fri Sept 19 2008 - v0.0.1
+- Initial release
+* Sun Sept 20 2008 - v0.0.2
+- bug fixed - max number of photos
+*/
+
+
+function widget_tumblr_recent_photos_init() {
+	if ( !function_exists('register_sidebar_widget') )
+		return;
+
+	function widget_tumblr_recent_photos( $args ) {
+		extract($args);
+
+		$options = get_option('widget_tumblr_recent_photos');
+		$title = $options['tbr_src_title'];
+		$tuid = $options['tbr_src-uid'];
+		$tbr_src_count = $options['tbr_src_count'];
+		$tbr_src_length = $options['tbr_src_length'];
+
+		$output = '<div id="tumblr_recent_photos">';
+
+		// section to get tumblr photos from here 
+		$request = 'http://' . $tuid . '.tumblr.com/api/read/json?num=999&type=photo';
+
+		$ci = curl_init($request);
+		curl_setopt($ci, CURLOPT_RETURNTRANSFER, TRUE);
+		$input = curl_exec($ci);
+
+		$input = str_replace('var tumblr_api_read = ','',$input);
+		$input = str_replace(';','',$input);
+
+		$value = json_decode($input, true);
+		$content =  $value['posts'];
+
+		$item = $tbr_src_count;	// the number of items you want to display
+		$type = 'photo-url-75';	// 75, 100, 250, 400, 500
+
+		$i = 0;$stopper = 0;$dispc = 0;
+		do {
+			if ($content[$i]['type'] == 'photo') {
+				$output .=  ('<a href="' . $content[$i]['url'] . '" target="_blank" ><img src="' . $content[$i][$type] . '" width="75" vspace = "4" hspace="4" alt="" title="" /></a>' );
+				if( $dispc % 2 ) { $output .= '<br />'; }
+				$dispc++;
+		    }
+			++$i;
+			if( ++$stopper > 100 ) { break; }	// make sure it will stop
+		} while( $dispc < $item );
+		if( $dispc == 0 ) $output .= "No photos.";
+		// tumblr section end
+
+		$output .= '</div>';
+
+		// These lines generate the output
+		echo $before_widget . $before_title . $title . $after_title;
+		echo $output;
+		echo $after_widget;
+	} /* widget_tumblr_recent_photos() */
+
+	function widget_tumblr_recent_photos_control() {
+		$options = $newoptions = get_option('widget_tumblr_recent_photos');
+		if ( $_POST["tbr_src-submit"] ) {
+			$newoptions['tbr_src_title'] = strip_tags(stripslashes($_POST["tbr_src_title"]));
+			$newoptions['tbr_src-uid'] = strip_tags(stripslashes($_POST["tbr_src-uid"]));
+			$newoptions['tbr_src_count'] = (int) $_POST["tbr_src_count"];
+			$newoptions['tbr_src_length'] = (int) $_POST["tbr_src_length"];
+		}
+		if ( $options != $newoptions ) {
+			$options = $newoptions;
+			update_option('widget_tumblr_recent_photos', $options);
+		}
+
+		// those are default value
+		if ( !$options['tbr_src_count'] ) $options['tbr_src_count'] = 4;
+		if ( !$options['tbr_src_length'] ) $options['tbr_src_length'] = 75;
+
+		$tbr_src_count = $options['tbr_src_count'];
+		$tbr_src_length = $options['tbr_src_length'];
+
+		$title = htmlspecialchars($options['tbr_src_title'], ENT_QUOTES);
+		$tuid = htmlspecialchars($options['tbr_src-uid'], ENT_QUOTES);
+?>
+
+	    <?php _e('Title:'); ?> <input style="width: 170px;" id="tbr_src_title" name="tbr_src_title" type="text" value="<?php echo $title; ?>" /><br />
+		<?php _e('Name:'); ?> <input style="width: 100px;" id="tbr_src-uid" name="tbr_src-uid" type="text" value="<?php echo $tuid; ?>" />.tumblr.com<br />
+            <p style="text-align: left;"><?php _e('Number of photos to show:'); ?> <input style="width: 20px;" id="tbr_src_count" name="tbr_src_count" type="text" value="<?php echo $tbr_src_count; ?>" /> 
+  	    <input type="hidden" id="tbr_src-submit" name="tbr_src-submit" value="1" />
+
+<?php
+	} /* widget_tumblr_recent_photos_control() */
+
+	// This registers our widget so it appears with the other available
+	// widgets and can be dragged and dropped into any active sidebars.
+	register_sidebar_widget('Tumblr Recent Photos', 'widget_tumblr_recent_photos');
+	register_widget_control('Tumblr Recent Photos', 'widget_tumblr_recent_photos_control' );
+} /* widget_tumblr_recent_photos_init() */
+
+// Run our code later in case this loads prior to any required plugins.
+add_action('plugins_loaded', 'widget_tumblr_recent_photos_init');
+
+?>
